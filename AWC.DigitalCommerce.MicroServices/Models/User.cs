1ca24adf63133febd8486c0c15f3e164de77623c
awcdeveloper-code <awcdigitalcommerce@gmail.com>
2025-01-26 11:37:44 -0600

    Added AWC.DigitalCommerce.MicroServices

---
+++ AWC.DigitalCommerce.MicroServices/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AWC.DigitalCommerce.MicroServices.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public int PIN { get; set; }
+        public string? UserName { get; set; }
+        public string? UserPassword { get; set; }
+        public int UserStatus { get; set; }
+    }
+}
